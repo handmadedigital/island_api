df7f46c472b02304ad0df178042067448d8dc987
rodrigo <rodrigo@threeaccents.com>
2015-05-31 22:39:08 -0400

    fix api

--- app/Products/Http/Transformers/ProductTransformer.php
+++ app/Products/Http/Transformers/ProductTransformer.php
@@ -1,10 +1,15 @@
 <?php namespace ThreeAccents\Products\Http\Transformers;
 
 use League\Fractal\TransformerAbstract;
+use ThreeAccents\Categories\Http\Transformers\CategoryTransformer;
 use ThreeAccents\Products\Entities\Product;
 
 class ProductTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = [
+        'categories'
+    ];
+
     public function transform(Product $product)
     {
         return [
@@ -14,5 +19,12 @@ class ProductTransformer extends TransformerAbstract
             'description' => $product->description,
             'image_src' => 'http://localhost:8000/media/product_images/'.$product->images[0]->src
         ];
+    }
+
+    public function includeCategories(Product $product)
+    {
+        $categories = $product->categories;
+
+        return $this->collection($categories, new CategoryTransformer(), 'categories');
     }
 }
--- app/Users/Http/routes.php
+++ app/Users/Http/routes.php
@@ -3,5 +3,5 @@
 $router->group(['prefix' => 'api/v1'], function($router){
     $router->post('/auth/login', ['uses' => 'Users\Http\Controllers\AuthController@postLogin']);
     $router->get('/users', ['uses' => 'Users\Http\Controllers\UserController@getUsers']);
-    $router->get('/user/{slug}', ['uses' => 'Users\Http\Controllers\UserController@getUser']);
+    $router->get('/users/{slug}', ['uses' => 'Users\Http\Controllers\UserController@getUser']);
 });
