2f22d5799ac3467cd0b7a91f49891178970e6094
rodrigo <rodrigo@threeaccents.com>
2015-06-04 17:49:44 -0400

    create container api

--- app/Shipping/Http/routes.php
+++ app/Shipping/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 $router->group(['prefix' => 'api/v1', 'middleware' => ['before' => 'jwt.auth']], function($router){
-    $router->get('/shipping-containers', ['uses' => 'Shipping\Http\Controllers\ShippingController@getShippingContainer']);
+    $router->get('/shippingContainers', ['uses' => 'Shipping\Http\Controllers\ShippingController@getShippingContainer']);
 });
