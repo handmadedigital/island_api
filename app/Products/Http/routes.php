525f1ee81511d23bde819a0366a45e63fab3d155
rodrigo <rodrigo@threeaccents.com>
2015-06-05 01:42:16 -0400

    fix route

--- app/Products/Http/routes.php
+++ app/Products/Http/routes.php
@@ -2,7 +2,7 @@
 
 $router->group(['prefix' => 'api/v1', 'middleware' => ['before' => 'jwt.auth']], function($router){
    $router->get('/products', ['uses' => 'Products\Http\Controllers\ProductController@getProducts']);
-   $router->post('/products', ['uses' => 'Products\Http\Controllers\ProductController@postAddProducts']);
+   $router->post('/products', ['uses' => 'Products\Http\Controllers\ProductController@postAddProduct']);
    $router->get('/products/{product_slug}', ['uses' => 'Products\Http\Controllers\ProductController@getProduct']);
    $router->get('/variants', ['uses' => 'Products\Http\Controllers\VariantController@getVariants']);
    $router->get('/variants/{variant_id}', ['uses' => 'Products\Http\Controllers\VariantController@getVariant']);
