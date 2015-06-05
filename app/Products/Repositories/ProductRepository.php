fa91c6465048766a19fa9bbec34cb661dd81e56c
rodrigo <rodrigo@threeaccents.com>
2015-06-05 01:38:14 -0400

    fix slug

--- app/Products/Repositories/ProductRepository.php
+++ app/Products/Repositories/ProductRepository.php
@@ -72,7 +72,7 @@ class ProductRepository
 
                 $model->name = $product->name;
                 $model->description = $product->description;
-                $model->slug = $product->name.rand(10000,99999);
+                $model->slug = $this->sluggify($product->name.rand(10000,99999));
 
                 $model->save();
 
@@ -91,5 +91,30 @@ class ProductRepository
                 $model->variants()->save($variant);
             }
         }
+    }
+
+    public function sluggify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        if (empty($text))
+        {
+            return 'n-a';
+        }
+
+        return $text;
     }
 }
