a507fb34350c3814b4d4982a77f5ace1796d1a3c
rodrigo <rodrigo@threeaccents.com>
2015-06-05 10:26:03 -0400

    add categroys lug

--- app/Categories/Http/Controllers/CategoryController.php
+++ app/Categories/Http/Controllers/CategoryController.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Input;
 use League\Fractal\Manager;
+use ThreeAccents\Categories\Entities\Category;
 use ThreeAccents\Categories\Http\Transformers\CategoryTransformer;
 use ThreeAccents\Categories\Services\CategoryService;
 use ThreeAccents\Core\Http\Controllers\ApiController;
+use ThreeAccents\Products\Http\Transformers\ProductTransformer;
 
 class CategoryController extends ApiController
 {
@@ -34,5 +36,14 @@ class CategoryController extends ApiController
         $categories = $this->service->getCategories();
 
         return  $this->respondWithCollection($categories, new CategoryTransformer, 'categories');
+    }
+
+    public function getCategory($category_slug)
+    {
+        $category = Category::where('slug', '=', $category_slug);
+
+        $products = $category->products;
+
+        return $this->respondWithCollection($products, new ProductTransformer());
     }
 }
--- app/Categories/Http/routes.php
+++ app/Categories/Http/routes.php
@@ -3,4 +3,5 @@
 $router->group(['prefix' => 'api/v1', 'middleware' => 'jwt.auth'], function($router)
 {
     $router->get('/categories', ['uses' => 'Categories\Http\Controllers\CategoryController@getCategories']);
+    $router->get('/{category_slug}/products', ['uses' => 'Categories\Http\Controllers\CategoryController@getCategory']);
 });
