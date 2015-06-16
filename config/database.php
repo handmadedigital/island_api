e1fe7f01ce7fd3adb985e667f084dac150c95336
rodrigo <rodrigo@threeaccents.com>
2015-06-16 14:30:47 -0400

    update config

--- config/database.php
+++ config/database.php
@@ -61,10 +61,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST'),
-			'database'  => env('DB_DATABASE'),
-			'username'  => env('DB_USERNAME'),
-			'password'  => env('DB_PASSWORD'),
+			'host'     => env('DB_HOST', $host),
+			'database' => env('DB_DATABASE', $database),
+			'username' => env('DB_USERNAME', $username),
+			'password' => env('DB_PASSWORD', $password),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
