7d3cca002592666e06c71b24bc857018aa8acd47
rodrigo <rodrigo@threeaccents.com>
2015-06-18 12:08:41 -0400

    fix that too

--- app/Products/Http/routes.php
+++ app/Products/Http/routes.php
@@ -2,7 +2,7 @@
 
 $router->group(['prefix' => 'api/v1'], function($router){
    $router->get('/products', ['uses' => 'Products\Http\Controllers\ProductController@getProducts']);
-   $router->get('/products/images', ['uses' => 'Products\Http\Controllers\ProductController@getProductImages']);
+   $router->get('/productImages', ['uses' => 'Products\Http\Controllers\ProductController@getProductImages']);
    $router->post('/products', ['uses' => 'Products\Http\Controllers\ProductController@postAddProduct']);
    $router->get('/products/{product_slug}', ['uses' => 'Products\Http\Controllers\ProductController@getProduct']);
    $router->get('/variants', ['uses' => 'Products\Http\Controllers\VariantController@getVariants']);
